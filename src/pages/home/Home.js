311fceeb9a3b4d3d864cc550048717b399b01e41
Touza2el <59705964+Touza2el@users.noreply.github.com>
2020-09-03 01:05:32 +0200

    ggg

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -9,6 +9,14 @@ function Home() {
       <Hero title='get comfortable room' btnText='our rooms' link='/rooms' />
       <Services />
       <NewRooms />
+      <div className='banner-section'>
+        <h1>
+          get comfortable room <span>with</span> <span>a</span> suitable price
+        </h1>
+      </div>
+      <div className='banner-section second'>
+        <h1>enjoy your time </h1>
+      </div>
     </div>
   );
 }
